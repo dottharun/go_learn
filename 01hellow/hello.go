11b4937d28e4a96439d0e58d68714055abfac8dc
ubutharun <tharun242424@gmail.com>
2023-12-02 14:26:41 +0530

    error handling

--- 01hellow/hello.go
+++ 01hellow/hello.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	//setting the std Logger:
+	//prefix for every log call
+	//and using flag 0(---enum like thing to where 0 means no flags) to disable logging time and date of the call
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// get the greeting string and print it
-	message := greetings.Hello("colaman")
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
--- 02greetings/greetings.go
+++ 02greetings/greetings.go
@@ -1,12 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // returns a greeting string for the name string
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 
 	//Returns a greeting that embeds a name in a message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }
