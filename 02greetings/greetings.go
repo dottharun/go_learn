2482e10459705755d6bf2e92a8d3b9e72621d44d
ubutharun <tharun242424@gmail.com>
2023-12-04 19:24:07 +0530

    random hello

--- 02greetings/greetings.go
+++ 02greetings/greetings.go
@@ -3,16 +3,27 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
-// returns a greeting string for the name string
+// Hello returns a greeting string for the name string
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
 
-	//Returns a greeting that embeds a name in a message
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// create a msg using random format
+	message := fmt.Sprintf(randomFormat(), name)
 
 	return message, nil
+}
+
+func randomFormat() string {
+	var formats = []string{
+		"Hi, %v, Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
